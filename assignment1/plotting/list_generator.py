4d97497224eb8612ef30ffba5e15b8e2cc502335
Alexander Kvalvaag <alexanderkvalvaag@gmail.com>
2026-01-24 13:37:26 +0100

    added gitignore, removed the largest datasets, replaced with intermediate points of 500, 5000, 50000 and 500000

--- assignment1/plotting/list_generator.py
+++ assignment1/plotting/list_generator.py
@@ -6,10 +6,12 @@ def generate_random_numbers(filename, count, min_value=0, max_value=1000):
             f.write(f"{random.randint(min_value, max_value)}\n")
 
 if __name__ == "__main__":
-    generate_random_numbers("../dataset/small/random_100.txt", 100, 0, 1000)
-    generate_random_numbers("../dataset/small/random_1000.txt", 1000, 0, 10000)
-    generate_random_numbers("../dataset/medium/random_10000.txt", 10000, 0, 100000)
-    generate_random_numbers("../dataset/medium/random_100000.txt", 100000, 0, 1000000)
-    generate_random_numbers("../dataset/large/random_1000000.txt", 1000000, 0, 1000000000)
-    generate_random_numbers("../dataset/large/random_10000000.txt", 10000000, 0, 1000000000)
+    generate_random_numbers("../dataset/small/random_100.txt", 100, 0, 1000000)
+    generate_random_numbers("../dataset/small/random_500.txt", 500, 0, 1000000)
+    generate_random_numbers("../dataset/small/random_1000.txt", 1000, 0, 1000000)
+    generate_random_numbers("../dataset/medium/random_5000.txt", 5000, 0, 1000000)
+    generate_random_numbers("../dataset/medium/random_10000.txt", 10000, 0, 1000000)
+    generate_random_numbers("../dataset/large/random_50000.txt", 50000, 0, 1000000)
+    generate_random_numbers("../dataset/large/random_100000.txt", 100000, 0, 1000000)
+    generate_random_numbers("../dataset/large/random_500000.txt", 500000, 0, 1000000)
     
